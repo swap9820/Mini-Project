7a07998428f4b83e4f9591b3a0b83085e34ba670
ishaaaaa <ishingote@gmail.com>
2019-10-05 21:15:53 +0530

    `

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -8,6 +8,6 @@ $dbName = "loginsystem";
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
 if(!$conn){
-}    die("Connection failed: ".mysqli_connect_error());
+   die("Connection failed: ".mysqli_connect_error());
 }
 ?>
