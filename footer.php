d0f41daafb6191c18c3bee4d50a478d11af37c44
rajatssss <54399934+rajatssss@users.noreply.github.com>
2019-10-09 22:46:38 +0530

    update footer

--- footer.php
+++ footer.php
@@ -156,7 +156,7 @@
   <!-- Footer Links -->
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+  <div class="footer-copyright text-center py-3">
     <a href="https://mdbootstrap.com/education/bootstrap/"></a>
   </div>
   <!-- Copyright -->
